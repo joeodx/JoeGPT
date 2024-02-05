720fdf97f99fad591076bf97ed35e7344e231066
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-02-05 22:55:50 +0000

    added

--- joe-chatgpd/components/SideBar.tsx
+++ joe-chatgpd/components/SideBar.tsx
@@ -1,12 +1,13 @@
+import NewChat from "./NewChat"
 
 
 function SideBar() {
   return (
-    <div className="p-2 flex flex-col h-screen">This is the sidebar
+    <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
           
-          {/*newchat*/}
+        <NewChat />
         <div>
           {/*ModelSelection */}
 
